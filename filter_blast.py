1f102a970499911e77983ea28588620d46cac443
严寒松 <hansongyan711@163.com>
2024-10-17 21:33:59 +0800

    simple_blast

--- filter_blast.py
+++ filter_blast.py
@@ -1,9 +1,13 @@
 import sys,os
+from collections import defaultdict
 
 
 
 def filter_blast(blast_out, fasta):
     dic_ref = {}
+    prefix = blast_out.split("/")[-1].split(".")[0]
+    out1 = open(f"{prefix}_stat_count.xls", "w")
+    out2 = open(f"{prefix}_filter_result.blast.out.xls", "w")
     seq = "seq"
     name = "name"
     for line in open(fasta):
@@ -16,6 +20,7 @@ def filter_blast(blast_out, fasta):
     dic_ref[name] = seq
 
     dic = {}
+    dicinfo = defaultdict(dict)
     for i in open(blast_out,'r').readlines():
         tmp = i.strip().split()
         blast_length = int(tmp[3])
@@ -23,16 +28,21 @@ def filter_blast(blast_out, fasta):
         mismatch = tmp[5]
         gap = [6]
         total = int(mismatch) + int(gap)
+        info = f"{blast_length}\t{mismatch}\t{gap}"
         if blast_length >= 17 and blast_length - total >= 15:
+            dicinfo[gene][i] = info
             if gene not in dic:
                 dic[gene] = 1
             else:
                 dic[gene] += 1
     for k in dic.keys():
-        print(f"{k}\t{dic[k]}\t{dic_ref[k]}")
+        out1.write(f"{k}\t{dic[k]}\t{dic_ref[k]}\n")
+    for key in dicinfo.keys:
+        for line in dicinfo[key].keys():
+            out2.write(line)
 
 
 if __name__ == '__main__':
     blast_out = sys.argv[1]
-    fasta= int(sys.argv[2])
+    fasta= sys.argv[2]
     filter_blast(blast_out, fasta)
